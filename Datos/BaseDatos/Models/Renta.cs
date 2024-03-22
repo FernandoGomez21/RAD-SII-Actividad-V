d0c900dd510a78208e334aff1c44b29ad209a66d
ferna <ferna@FERNANDOGOMEZ>
2024-03-21 22:25:30 -0600

    Add Tablas

---
+++ Datos/BaseDatos/Models/Renta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos.Models
+{
+    public class Renta
+    {
+        [Key]
+        public int RentaId { get; set; }
+        public int clienteId { get; set; }
+        public Clientes cliente { get; set; }
+        public int PeliculasId { get; set; }
+        public Peliculas Peliculas { get; set; }
+        [Required]
+        public DateTime FechaRenta { get; set; }
+        [Required]
+        public DateTime FechaRetorno { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioRenta { get; set; }
+
+    }
+}
