3f4bd2574644e3d2c10354e0dfdc13aeae079e6a
ferna <ferna@FERNANDOGOMEZ>
2024-03-21 22:46:01 -0600

    Agg Clase DPeliculas en Datos

---
+++ Datos/DPeliculas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.BaseDatos.Models;
+using Datos.Core;
+
+namespace Datos
+{
+    public class DPeliculas
+    {
+        UnitOfWork _unitOfWork;
+
+        public DPeliculas()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+        public int PeliculasId { get; set; }
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+        public string Autores { get; set; }
+        public int Existencia { get; set; }
+        public decimal PrecioRenta { get; set; }
+        public bool Estado { get; set; }
+
+        public List<Peliculas> categoriaClientesTodas()
+        {
+            return _unitOfWork.Repository<Peliculas>().Consulta().ToList();
+        }
+
+        public int GuardarCategoria(Peliculas categoria)
+        {
+            if (categoria.PeliculasId == 0)
+            {
+                _unitOfWork.Repository<Peliculas>().Agregar(categoria);
+                return _unitOfWork.Guardar();
+            }
+            else
+            {
+                var CategoriaInDb = _unitOfWork.Repository<Peliculas>().Consulta().FirstOrDefault(c => c.PeliculasId == categoria.PeliculasId);
+
+                if (CategoriaInDb != null)
+                {
+                    CategoriaInDb.Nombre = categoria.Nombre;
+                    CategoriaInDb.Genero = categoria.Genero;
+                    CategoriaInDb.Autores = categoria.Autores;
+                    CategoriaInDb.Existencia = categoria.Existencia;
+                    CategoriaInDb.PrecioRenta = categoria.PrecioRenta;
+                    CategoriaInDb.Estado = categoria.Estado;
+                    _unitOfWork.Repository<Peliculas>().Editar(categoria);
+                    return _unitOfWork.Guardar();
+                }
+                return 0;
+            }
+
+        }
+
+        public int EliminarCategoria(int categoriaId)
+        {
+            var CategoriaInDb = _unitOfWork.Repository<Peliculas>().Consulta().FirstOrDefault(c => c.PeliculasId == categoriaId);
+            if (CategoriaInDb != null)
+            {
+                _unitOfWork.Repository<Peliculas>().Eliminar(CategoriaInDb);
+                return _unitOfWork.Guardar();
+            }
+            return 0;
+        }
+
+
+
+    }
+}
